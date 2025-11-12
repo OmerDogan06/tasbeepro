919c4e83ddcabb8209614f140308260ed41f4743
omer <omer.dogan006@hotmail.com>
2025-11-12 13:25:30 +0300

    fix: Remove unused import from QuranReadingScreen

--- lib/screens/quran_reading_screen.dart
+++ lib/screens/quran_reading_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'dart:math' as math;
 import '../constants/sura_names.dart';
 import '../l10n/app_localizations.dart';
